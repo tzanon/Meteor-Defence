427973b49395fe3ae2e287ff6b5df4ec6f4d9918
Tyler Zanon <tdzanon@gmail.com>
2022-03-19 01:04:10 -0400

    added player first-person controls, adapted debug controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+	private MeteorDefencePlayer _controlMapping;
+
+	private Vector3 _movementDirection;
 	private float _movementSpeed = 2f;
 
+	private float _xMouse, _yMouse;
+	private float _lookSensitivity = 8f;
 	private Camera _pov;
 
 	private void Awake()
 	{
 		_pov = GetComponentInChildren<Camera>();
-	}
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		
-	}
+		_xMouse = transform.rotation.eulerAngles.y;
+		_yMouse = _pov.transform.rotation.eulerAngles.x;
 
-	// Update is called once per frame
-	void Update()
-	{
-		
+		_movementDirection = Vector3.zero;
+
+		_controlMapping = new MeteorDefencePlayer();
+		//_controlMapping.Player.Move
 	}
 
 	private void FixedUpdate()
 	{
-		
+		if (_movementDirection != Vector3.zero)
+		{
+			transform.Translate(_movementSpeed * Time.fixedDeltaTime * _movementDirection);
+		}
 	}
+
+	public void Move(InputAction.CallbackContext context)
+	{
+		var moveVal = context.ReadValue<Vector2>();
+
+		if (context.performed)
+		{
+			_movementDirection = new Vector3(moveVal.x, 0f, moveVal.y);
+		}
+		else if (context.canceled)
+		{
+			_movementDirection = Vector3.zero;
+		}
+	}
+
+	public void Look(InputAction.CallbackContext context)
+	{
+		var mouseDelta = context.ReadValue<Vector2>();
+
+		float deltaX = mouseDelta.x * _lookSensitivity * Time.deltaTime;
+		float deltaY = mouseDelta.y * _lookSensitivity * Time.deltaTime;
+
+		_xMouse += deltaX;
+		_yMouse = Mathf.Clamp(_yMouse - deltaY, -80f, 80f);
+
+		_pov.transform.localEulerAngles = new Vector3(_yMouse, 0f, 0f);
+		transform.localEulerAngles = new Vector3(0f, _xMouse, 0f);
+
+		if (context.performed)
+		{
+			//Debug.Log("Performed look");
+		}
+		else if (context.canceled)
+		{
+			//Debug.Log("Canceled look");
+		}
+	}
+
+	public void FireWeapon(InputAction.CallbackContext context)
+	{
+		if (context.performed)
+		{
+			Debug.Log("Performed firing");
+		}
+		else if (context.canceled)
+		{
+			Debug.Log("Canceled firing");
+		}
+	}
+
+	public void NextWeapon(InputAction.CallbackContext context)
+	{
+
+	}
+
+	public void PrevWeapon(InputAction.CallbackContext context)
+	{
+
+	}
+
+	public void NextEquipment(InputAction.CallbackContext context)
+	{
+
+	}
+
+	public void PrevEquipment(InputAction.CallbackContext context)
+	{
+
+	}
+
 }
--- Assets/Scripts/RadarObjectDetector.cs
+++ Assets/Scripts/RadarObjectDetector.cs
@@ -1,51 +1,49 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RadarObjectDetector : MonoBehaviour
 {
+	private MeteorDefencePlayer _controls;
+
 	public RadarScreen screen;
 
 	public GameObject dummyEnemy;
 	private int _dummyIdx = 1;
 
 	private float _rotateRate = 20f;
+	private Vector3 _rotation = Vector3.zero;
+
+	private void OnEnable()
+	{
+		_controls.Debug.Enable();
+	}
+
+	private void OnDisable()
+	{
+		_controls.Debug.Disable();
+	}
+
+	private void Awake()
+	{
+		_controls = new MeteorDefencePlayer();
+		_controls.Debug.SpawnMeteor.performed += SpawnMeteor;
+
+		_controls.Debug.RotateRadar.started += RotateRadar;
+		_controls.Debug.RotateRadar.canceled += RotateRadar;
+	}
 
 	// Start is called before the first frame update
-	void Start()
+	private void Start()
 	{
 		screen = GetComponentInChildren<RadarScreen>();
 	}
 
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Return))
-		{
-			var dummy = Instantiate(dummyEnemy);
-
-			float xCoord = Random.Range(-80f, 60f);
-			Vector3 startPos = new Vector3(xCoord, 10f, 120f);
-
-			dummy.transform.position = startPos;
-			//dummy.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-			dummy.name = "dummy " + _dummyIdx;
-			_dummyIdx++;
-
-			//screen.AddObjectToScreen(dummy);
-		}
-	}
-
 	private void FixedUpdate()
 	{
-		var rotation = new Vector3(0f, _rotateRate, 0f) * Time.fixedDeltaTime;
-
-		if (Input.GetKey(KeyCode.RightArrow))
+		if (_rotation != Vector3.zero)
 		{
-			transform.Rotate(rotation, Space.World);
-		}
-
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.Rotate(-rotation, Space.World);
+			transform.Rotate(_rotation * Time.deltaTime);
 		}
 	}
 
@@ -62,6 +60,32 @@ public class RadarObjectDetector : MonoBehaviour
 		if (other.GetComponent<Trackable>())
 		{
 			screen.RemoveObjectFromScreen(other.gameObject);
+		}
+	}
+
+	private void SpawnMeteor(InputAction.CallbackContext ctx)
+	{
+		var dummy = Instantiate(dummyEnemy);
+
+		float xCoord = Random.Range(-80f, 60f);
+		Vector3 startPos = new Vector3(xCoord, 10f, 120f);
+
+		dummy.transform.position = startPos;
+		//dummy.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+		dummy.name = "dummy " + _dummyIdx;
+		_dummyIdx++;
+	}
+
+	private void RotateRadar(InputAction.CallbackContext ctx)
+	{
+		if (ctx.started)
+		{
+			var direction = ctx.ReadValue<float>();
+			_rotation = new Vector3(0f, _rotateRate * direction, 0f);
+		}
+		else if (ctx.canceled)
+		{
+			_rotation = Vector3.zero;
 		}
 	}
 
